4a0b8de21ad69381d140866329aeb8bd1d6f8dbf
stitch1280 <blay4480@gmail.com>
2016-01-31 21:18:05 -0700

    Renamed the plugin and got it to be recognized by Glue.

---
+++ Glue with PurchasingManager/PurchasingManagerPlugin/PurchasingManagerStartup.cs
@@ -0,0 +1,49 @@
+﻿using FlatRedBall.Glue.Plugins;
+using FlatRedBall.Glue.Plugins.ExportedInterfaces;
+using FlatRedBall.Glue.Plugins.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PurchasingManagerPlugin
+{
+    [Export(typeof(PluginBase))]
+    public partial class PurchasingManagerStartup :PluginBase
+    {
+        [Import("GlueCommands")]
+        public IGlueCommands GlueCommands
+        {
+            get;
+            set;
+        }
+
+        [Import("GlueState")]
+        public IGlueState GlueState { get; set; }
+
+        public override string FriendlyName
+        {
+            get { return "Purchasing Manager"; }
+        }
+
+        public override Version Version
+        {
+            get { return new Version(1, 0); }
+        }
+
+
+        public override void StartUp()
+        {
+
+        }
+
+        public override bool ShutDown(PluginShutDownReason shutDownReason)
+        {
+
+
+            return true;
+        }
+    }
+}
